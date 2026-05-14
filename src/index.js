af53bed52af0beb18713a7500d0911c40bb4cd6c
Mayank <yourgithubemail@gmail.com>
2026-05-14 19:03:36 +0530

    Added Vercel CORS

--- src/index.js
+++ src/index.js
@@ -22,7 +22,11 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://your-vercel-app.vercel.app'
+  ],
   credentials: true,
 }));
 app.use(express.json());
