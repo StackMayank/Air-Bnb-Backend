9102de987be7d98da6bc26ea00ab02110cdc2c9e
Mayank <yourgithubemail@gmail.com>
2026-05-15 10:58:00 +0530

    fixed cors

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://your-vercel-app.vercel.app'
+    'https://staybookers.vercel.app'
   ],
   credentials: true,
 }));
